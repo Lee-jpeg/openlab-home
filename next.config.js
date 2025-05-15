c2525e87ea5a6f6e6c2cea55d0e04d8c5acb694a
이승우 Macbook Pro <leesw@iseung-uui-MacBookPro.local>
2025-05-15 22:20:20 +0900

    정적 사이트만 배포

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  distDir: "out",
+  images: { unoptimized: true },
+  basePath: "/openlab-home",
+  assetPrefix: "/openlab-home/",
+  trailingSlash: true,
 }
-
-module.exports = nextConfig 
+module.exports = nextConfig
